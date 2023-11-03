dab0604eec698cc80bbdc2249c54841846a8e60c
NettoJM <nettojm86@gmail.com>
2023-11-02 23:08:10 -0300

    Delete comment

--- app/backend/src/routes/leaderboard.route.ts
+++ app/backend/src/routes/leaderboard.route.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import { LeaderboardController } from '../controllers';
-// import { AuthenticationMiddleware, LoginMiddleware } from '../middlewares';
 
 const leaderboardRoute = Router();
 
